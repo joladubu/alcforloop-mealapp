427f2a81257e44164bb6d262dde5556b089cde99
Ajibola Oladubu <joladubu@gmail.com>
2019-02-17 13:10:59 +0100

    installed nodemon

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 9001;
+
+app.get('/', (req, res) => {
+  return res.send('The API is working');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
