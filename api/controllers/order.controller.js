705c8f886b4c3a241e39aa3eaadb7ac4fb2bb95d
Ajibola Oladubu <joladubu@gmail.com>
2019-02-18 10:16:03 +0100

    Created Order Controller

---
+++ api/controllers/order.controller.js
@@ -0,0 +1,29 @@
+import OrderService from '../services/order.service';
+
+const OrderController = {
+    getAllOrders(req, res){
+        const allOrders = OrderService.getAllOrders();
+        return res.json({
+            status: 'success',
+            data: allOrders
+        }).status(200);
+    },
+    addOrder (req, res){
+       const newOrder = req.body;
+       const createdOrder = OrderService.addOrder(newOrder);
+       return res.json({
+            status: 'success',
+            data: createdOrder,
+        }).status(201);
+    },
+    updateAnOrder(req, res) {
+        const orderToUpdate = req.body;
+        const updatedOrder = OrderService.updateOrder(orderToUpdate);
+        return res.json({
+          status: 'success',
+          data: updatedOrder,
+        }).status(201);
+      }
+}
+
+export default OrderController;
