1e587e0b43984016d2f6c9908e6b3c5190ad468d
Ajibola Oladubu <joladubu@gmail.com>
2019-02-18 04:09:52 +0100

    Created Meal Service

---
+++ api/utils/dummyData.js
@@ -0,0 +1,27 @@
+export default {
+    meals: [
+        {
+            id: 1,
+            name: 'Fried Rice',
+            size: 'Medium',
+            price: '450'
+        },
+        {
+            id: 2,
+            name: 'JOllof Rice',
+            size: 'Large',
+            price: '550'
+        },
+        {
+            id: 3,
+            name: 'Beams',
+            size: 'Small',
+            price: '150'
+        },        {
+            id: 4,
+            name: 'Eba and Semo',
+            size: 'Medium',
+            price: '300'
+        }
+    ]
+}
